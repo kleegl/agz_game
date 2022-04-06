11c0d64e77c06687ec078859358ef716bd453153
Jenya_Mefedov <jenya-mefedov@mail.ru>
2022-04-06 18:09:39 +0300

    commit
    add also destroy method

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,13 +1,30 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    private GameObject ground;
+    private Camera _camera;
+
+    private void Start()
+    {
+        _camera = Camera.main;
+        ground = GameObject.FindGameObjectWithTag("Ground");
+    }
+
     private void OnCollisionEnter2D(Collision2D other)
     {
+        print(other.transform.position);
         if (other.transform.CompareTag("Ground"))
+            Destroy(this.gameObject);
+    }
+
+    private void Update()
+    {
+        if(transform.position.y < ground.transform.position.y)
             Destroy(this.gameObject);
     }
 }
