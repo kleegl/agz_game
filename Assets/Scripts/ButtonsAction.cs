43804e1b9c8ecf463a3a6608d101c77a2099d974
Jenya_Mefedov <jenya-mefedov@mail.ru>
2022-04-11 19:27:51 +0300

    commit

---
+++ Assets/Scripts/ButtonsAction.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ButtonsAction : MonoBehaviour
+{
+    private void OpenHitHubLink()
+    {
+        Application.OpenURL("https://github.com/JenyaMefedov/agz");
+    }
+}
