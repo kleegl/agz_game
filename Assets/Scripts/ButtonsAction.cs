7d7a18a8a373c3ddf20c43e4019f4a686e241346
Jenya_Mefedov <jenya-mefedov@mail.ru>
2022-04-11 21:13:03 +0300

    commit
    add Menu scene, events, animation on touch for button, open links github and twitter

--- Assets/Scripts/ButtonsAction.cs
+++ Assets/Scripts/ButtonsAction.cs
@@ -2,12 +2,55 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
-public class ButtonsAction : MonoBehaviour
+public class ButtonsAction : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
-    private void OpenHitHubLink()
+    public GameObject twitter;
+    public GameObject git;
+    public GameObject start;
+    public GameObject options;
+    public GameObject quit;
+    
+    
+    
+    //сделать родительский класс для всех кнопок и override функции 
+    // почитать про ивенты
+
+    public void OpenHitHubLink()
     {
         Application.OpenURL("https://github.com/JenyaMefedov/agz");
+    }
+
+    public void OpenTwitterLink()
+    {
+        Application.OpenURL("https://twitter.com/j_mefedov");
+    }
+
+    public void OnStart()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void OnOptions()
+    {
+        print("option");
+    }
+
+    public void OnQuit()
+    { 
+        Application.Quit();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        eventData.selectedObject.transform.localScale *= 0.8f;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        eventData.selectedObject.transform.localScale /= 0.8f;
     }
 }
