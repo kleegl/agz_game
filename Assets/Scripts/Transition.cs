0e24a225538656a66583fbd392ecb9a8e9d12a91
Jenya_Mefedov <jenya-mefedov@mail.ru>
2022-04-12 12:31:04 +0300

    add transistion script
    need to do transistion script

---
+++ Assets/Scripts/Transition.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transition : MonoBehaviour
+{
+    public Animation animator;
+    public float transistionDelayTime = 0.2f;
+
+    private void Awake()
+    {
+        animation = gameObject.Find()
+    }
+
+    private void Start()
+    {
+            
+    }
+}
