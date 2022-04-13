4350a0ee72d8c13f69fd574edbe0ba18975e3b14
Jenya_Mefedov <jenya-mefedov@mail.ru>
2022-04-13 10:16:11 +0300

    done transitions
    done in-out scene's transitions

--- Assets/Scripts/Buttons/PlayButton.cs
+++ Assets/Scripts/Buttons/PlayButton.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,8 +7,23 @@ using UnityEngine.UIElements;
 
 public class PlayButton : Buttons
 {
+    public Animator animator;
+    public float transistionDelayTime = 5f;
+
+    private void Awake()
+    {
+        animator = GameObject.Find("Transition").GetComponent<Animator>();
+    }
+
     public void OnPlay()
     {
+        StartCoroutine(DelayLoadScene());
+    }
+
+    IEnumerator DelayLoadScene()
+    {
+        animator.Play("TransitionOut");
+        yield return new WaitForSeconds(transistionDelayTime);
         SceneManager.LoadScene("Game");
     }
 }
