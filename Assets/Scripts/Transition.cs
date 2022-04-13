68713555d834a16150a551e69b5ea3382e1ec2b9
Jenya_Mefedov <jenya-mefedov@mail.ru>
2022-04-13 10:05:14 +0300

    add transition
    add start transition

--- Assets/Scripts/Transition.cs
+++ Assets/Scripts/Transition.cs
@@ -2,19 +2,27 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.SceneManagement;
 
 public class Transition : MonoBehaviour
 {
-    public Animation animator;
+    public Animator animator;
     public float transistionDelayTime = 0.2f;
 
     private void Awake()
     {
-        animation = gameObject.Find()
+        animator = GameObject.Find("Transition").GetComponent<Animator>();
     }
 
     private void Start()
     {
-            
+        StartCoroutine(DelayLoadTransition());
+    }
+
+    IEnumerator DelayLoadTransition()
+    {
+        animator.Play("TransitionIn");
+        yield return new WaitForSeconds(transistionDelayTime);
     }
 }
